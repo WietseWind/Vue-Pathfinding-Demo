249670cabf51e569baaeba80c62478ebffb66440
Wietse Wind <mail@wietse.com>
2022-05-23 09:43:41 +0200

    Remove Bootstrap -non precompiled version

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
 
 const app = createApp(App);
 app.mount("#app");
